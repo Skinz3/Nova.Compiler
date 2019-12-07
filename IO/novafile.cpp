5015252a5633e448649adf44dca7e2121fb41933
Skinz <axiom1008@gmaiil.com>
2019-12-07 14:43:04 +0100

    ReadLine() from NovaFile.cpp

--- IO/novafile.cpp
+++ IO/novafile.cpp
@@ -6,7 +6,10 @@ using namespace std;
 NovaFile::NovaFile(string fileName)
 {
     this->fileName = fileName;
-
+    this->ReadLines();
+}
+void NovaFile::ReadLines()
+{
     ifstream fstream(fileName);
 
     int lineCount = count(istreambuf_iterator<char>(fstream), istreambuf_iterator<char>(), '\n') + 1;
@@ -15,20 +18,12 @@ NovaFile::NovaFile(string fileName)
 
     this->lines = new vector<string>();
 
-    int i = 0;
     string line;
 
     while (getline(fstream, line))
     {
         lines->push_back(line);
-        
     }
-
-    for (int i = 0;i < lines->size();i++)
-    {
-       cout << lines->at(i) << endl;
-    }
-
 
     fstream.close();
 }
--- IO/novafile.h
+++ IO/novafile.h
@@ -11,5 +11,5 @@ public:
     
 private:
     vector<string>* lines;
-
+    void ReadLines();
 };
