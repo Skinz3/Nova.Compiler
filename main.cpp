491d8e098d4bf8dc014d9fde5349a2f7b9504b7e
Skinz <axiom1008@gmaiil.com>
2019-12-07 05:05:15 +0100

    how is the project working.png

--- main.cpp
+++ main.cpp
@@ -7,40 +7,32 @@
 
 using namespace std;
 
-
-
 int main(int argc, char *argv[])
 {
-   
-
     string fileName = argv[1];
-    
-    cout <<  fileName  << endl;
 
-    ifstream fstream(fileName); 
+    cout << fileName << endl;
 
-    int lineCount =  count(istreambuf_iterator<char>(fstream), istreambuf_iterator<char>(), '\n') + 1;
-   
+    ifstream fstream(fileName);
+
+    int lineCount = count(istreambuf_iterator<char>(fstream), istreambuf_iterator<char>(), '\n') + 1;
+
     fstream.seekg(0, ios::beg); // seek to begining
 
-    string* lines = new string[lineCount]; 
-    
+    string *lines = new string[lineCount];
 
     int i = 0;
     std::string line;
 
     while (getline(fstream, line))
     {
-        
+
         lines[i] = line;
         i++;
-       
     }
 
- 
-    
-     NovaFile* file = new NovaFile("test");
-     cout << file->fileName << endl;
+    NovaFile *file = new NovaFile("test");
+    cout << file->fileName << endl;
 
     system("pause");
     return EXIT_SUCCESS;
