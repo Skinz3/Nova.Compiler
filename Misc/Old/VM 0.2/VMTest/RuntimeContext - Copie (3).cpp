80c9793c8eb3fada9f61f995f865fedc6312a143
Skinz3 <oliviamarius35@gmail.com>
2020-04-08 05:33:43 +0200

    Some old revisions

---
+++ Misc/Old/VM 0.2/VMTest/RuntimeContext - Copie (3).cpp
@@ -0,0 +1,35 @@
+#include "RuntimeContext.h"
+
+
+void RuntimeContext::PushInt(int value)
+{
+	stack.emplace_back(value);
+}
+void RuntimeContext::PushStack(RuntimeContext::RuntimeElement & value)
+{
+	stack.emplace_back(value);
+}
+
+void RuntimeContext::PushString(std::string* value)
+{
+	stack.emplace_back(value);
+}
+
+int RuntimeContext::PopStackInt()
+{
+	return std::get<int>(this->PopStack());
+}
+
+
+
+RuntimeContext::RuntimeElement RuntimeContext::PopStack()
+{
+	size_t stackSize = stack.size();
+	RuntimeElement value = stack.at(stackSize - 1);
+	stack.erase(stack.begin() + stackSize - 1);
+	return value;
+}
+
+ 
+
+ 
